c650f3c30694b5e97b4e81b41b84464311871ef9
spiderman <280374217@qq.com>
2026-05-20 19:27:09 +0800

    m

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from '@/framework/index'
+import router from '@/framework/router'
 import '@/styles/index.css'
 
 createApp(App).use(router).mount('#app')
